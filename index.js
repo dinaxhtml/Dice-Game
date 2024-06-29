4dda185fd100ddc88ec06540984c8f5ceb1e2051
Dina Osama <108067899+dinaxhtml@users.noreply.github.com>
2024-06-29 17:43:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 =Math.floor(Math.random()*6)+1;
+var randomNumber2 =Math.floor(Math.random()*6)+1;
+
+document.querySelector("button").addEventListener("click", function() {
+
+    var randomNumber1 =Math.floor(Math.random()*6)+1;
+    var randomNumber2 =Math.floor(Math.random()*6)+1;
+
+    var p1 =document.querySelector(".img1").setAttribute("src", "./images/dice"+randomNumber1+".png");
+    var p2 =document.querySelector(".img2").setAttribute("src", "./images/dice"+randomNumber2+".png");
+
+    if(randomNumber1>randomNumber2) {   
+        document.querySelector("h1").innerHTML="Player 1 Wins 🏆";
+    } else if(randomNumber2>randomNumber1) {
+        document.querySelector("h1").innerHTML="Player 2 Wins 🏆";
+    } else {
+        document.querySelector("h1").innerHTML="Draw ⚔️";
+    }
+});
